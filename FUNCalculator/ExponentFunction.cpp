becc493c5d8c7a3bef9589c656d37aacb6e3e098
agafonovmak <agafonovmak@gmail.com>
2014-10-20 22:42:22 +0400

    implemented exponent function yo

--- FUNCalculator/ExponentFunction.cpp
+++ FUNCalculator/ExponentFunction.cpp
@@ -1,11 +1,31 @@
 #include "ExponentFunction.h"
-
+#include<iostream>
+using std::cin;
+using std::cout;
+using std::endl;
 
 ExponentFunction::ExponentFunction()
 {
+	m_name = "y=e^x";
 }
 
 
 ExponentFunction::~ExponentFunction()
 {
+}
+
+void ExponentFunction::Calculate()
+{
+	cout << "Input variable";
+	cin >> m_x;
+	double member = 1;
+	double sum = 1;
+	int n = 1;
+	while (member >= 0, 000001)
+	{
+		member = member*m_x / n;
+		sum += member;
+		n++;
+	}
+	cout << sum << endl;
 }
